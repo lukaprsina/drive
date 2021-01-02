efba94596bae33936597daa4ff06ac97b14068fc
Luka Pršina <luk.prsina@gmail.com>
2021-01-02 13:10:18 +0100

    windows no use-gesture for inventory
    before dnd

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
-import { useGesture } from "react-use-gesture";
 import { IconButton } from "@material-ui/core";
 
 import forward from "./img/forward.svg";
@@ -17,29 +15,9 @@ export function Inventory() {
 }
 
 function Item(props) {
-  const [{ x, y }, setSpring] = useSpring(() => ({
-    x: 0,
-    y: 0,
-  }));
-
-  const bind = useGesture(
-    {
-      onDrag: ({ down, movement: [mx, my] }) => {
-        setSpring({ x: down ? mx : 0, y: down ? my : 0 });
-      },
-
-      onDragEnd: ({ tap }) => {
-        console.log(tap);
-      },
-    },
-    { filterTaps: true }
-  );
-
-  const AnimatedIcon = animated(IconButton);
-
   return (
-    <AnimatedIcon className="drag" {...bind()} style={{ x, y }}>
+    <IconButton className="drag">
       <img src={props.src} className="sign" alt={props.src} draggable={false} />
-    </AnimatedIcon>
+    </IconButton>
   );
 }
